0dbc4beb0cf7d5b2205fa9238c0aa0dfcd907d73
Oleksa <oleksa.antsupov@gmail.com>
2022-04-11 20:43:35 +0300

    Added more kinds of loop.

--- js/script.js
+++ js/script.js
@@ -23,6 +23,38 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+/* let i = 0;
+
+while (i < 2) {
+    const a = prompt('One of the last movies you watched?', ''),
+          b = prompt('How much would you rate it?', '');
+    i++;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+} */
+
+/* let i = 0;
+
+do {
+    const a = prompt('One of the last movies you watched?', ''),
+          b = prompt('How much would you rate it?', '');
+    i++;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+while (i < 2); */
+
+
 if (personalMovieDB.count < 10) {
     console.log('Few films watched');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
